48a9bf324a766b19571a4df49fa37211375f5312
taoyuan <torworx@gmail.com>
2015-07-15 17:52:06 +0800

    refactor

--- test/middist-handle.test.js
+++ test/middist-handle.test.js
@@ -7,37 +7,71 @@ var middist = require('../');
 
 describe('middist/handle', function () {
 
+    function plus(args, result) {
+        var answer = 0;
+        args.forEach(function (arg) {
+            answer += arg;
+        });
+        result.push(answer);
+    }
+
+    function minus(args, result) {
+        var answer = args.shift();
+        args.forEach(function (arg) {
+            answer -= arg;
+        });
+        result.push(answer);
+    }
+
+    function multiply(args, result, done) {
+        var answer = 1;
+        args.forEach(function (arg) {
+            answer *= arg;
+        });
+        result.push(answer);
+        process.nextTick(done);
+    }
+
+    function divide(args, result, done) {
+        var answer = args.shift();
+        args.forEach(function (arg) {
+            if (!arg) throw new Error('divide error');
+            answer /= arg;
+        });
+        result.push(answer);
+        process.nextTick(done);
+    }
+
+    var handle = middist.build(function (operator, data, next) {
+        data.result = data.result || [];
+        if (operator.length > 2) {
+            operator(data.args.slice(), data.result, next);
+        } else {
+            operator(data.args.slice(), data.result);
+            next();
+        }
+    });
+
     it('should handle function stack', function (done) {
-        var answer = [];
-
-        function plus(a, b) {
-            answer.push(a + b);
-        }
-
-        function minus(a, b) {
-            answer.push(a - b);
-        }
-
-        function multiply(a, b, cb) {
-            answer.push(a * b);
-            cb();
-        }
-
-        function divide(a, b, done) {
-            if (!b) throw new Error('divide error');
-            answer.push(a / b);
-            done();
-        }
-
 
         var p = pd.plan(2, done);
-        middist.handle([plus, minus, multiply, divide], [10, 5], function (err) {
+
+        var operators = [plus, minus, multiply, divide];
+
+
+        var data = {
+            args: [10, 5]
+        };
+        handle(operators, data, function (err) {
             t.notOk(err);
-            t.deepEqual(answer, [10 + 5, 10 - 5, 10 * 5, 10 / 5]);
+            t.deepEqual(data.result, [10 + 5, 10 - 5, 10 * 5, 10 / 5]);
             p.check();
         });
 
-        middist.handle([plus, minus, multiply, divide], [10, 0], function (err) {
+        var data2 = {
+            args: [10, 0]
+        };
+        handle(operators, data2, function (err) {
             p.ok(err);
             p.check();
         });
